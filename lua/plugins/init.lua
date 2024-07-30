104ad578fd29c669a0bb27aabf7d6a500d14f63f
Akhmad Salafudin <axmad386@gmail.com>
2024-07-30 14:04:23 +0700

    feat: upgrade astronvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,20 +1,22 @@
 return {
   -- buat GIT
-  { "tpope/vim-fugitive",     cmd = "G" },
+  { "tpope/vim-fugitive", cmd = "G" },
 
-  { "fatih/vim-go",           ft = "go" },
-  { "jwalton512/vim-blade",   ft = "blade" },
+  { "fatih/vim-go", ft = "go" },
+  { "jwalton512/vim-blade", ft = "blade" },
 
   -- Semacam BetterPest kalau di VsCode
-  { "vim-test/vim-test",      ft = { "php", "typescript" } },
+  { "vim-test/vim-test", ft = { "php", "typescript" } },
 
   -- untuk replace kata otomatis sesuai casenya
-  { "tpope/vim-abolish",      cmd = { "S", "Subvert" } },
+  { "tpope/vim-abolish", cmd = { "S", "Subvert" } },
 
   -- untuk search word di banyak file atau folder
-  { 'mileszs/ack.vim',        lazy = false },
+  { "mileszs/ack.vim", lazy = false },
 
   -- untuk delete list di quicklist dengan dd
-  { 'TamaMcGlinn/quickfixdd', cmd = "Ack" },
-  { 'lbrayner/vim-rzip' },
+  { "TamaMcGlinn/quickfixdd", cmd = "Ack" },
+  { "lbrayner/vim-rzip" },
+  { "projekt0n/caret.nvim" },
+  { "tpope/vim-rails", ft = { "ruby", "eruby" } },
 }
